371cf4cc4ddeafe316d5ab251b775647f6b5b0aa
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2024-01-27 23:36:31 -0600

    added ContactRow.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import React from "react";
+import ContactList from "./components/ContactList";
+import dummyContacts from "./components/ContactList";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
 
 export default App;
